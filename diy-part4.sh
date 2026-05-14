1f7412006756cd1a1eccc5628f70abc741726de8
docoloa <goforreg@outlook.com>
2026-05-14 11:47:02 +0800

    Update diy-part4.sh

--- diy-part4.sh
+++ diy-part4.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 
 # turboacc
 curl -sSL https://raw.githubusercontent.com/mufeng05/turboacc/main/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
@@ -11,7 +11,7 @@ git clone https://github.com/gSpotx2f/luci-app-temp-status package/luci-app-temp
 git clone https://github.com/gSpotx2f/luci-app-cpu-perf package/luci-app-cpu-perf
 
 # OpenClash
-git clone --depth 1 https://github.com/vernesong/OpenClash.git OpenClash
+# git clone --depth 1 https://github.com/vernesong/OpenClash.git OpenClash
 
 # luci-app-airoha-npu
 if [ ! -d "package/luci-app-airoha-npu" ]; then
